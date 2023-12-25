0df107ccf167f48a6f41b958b75e7e44039d2e56
limshixun <limshixun1106@gmail.com>
2023-12-25 22:46:44 +0800

    Sign in, sign up and sign up successful implemented

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from os import listdir
 from flask_mysqldb import MySQL
+import bcrypt 
 
 app = Flask(__name__)
 
@@ -13,38 +13,68 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # IF post request
+    # Error message
+    message = ""
     if request.method == 'POST':
-        # fetch the user input from html
+        # Get user input from <form>
         username = request.form['username']
-        pword = request.form['password']
+        # turn password into bytes
+        pword = request.form['password'].encode('utf-8')
 
-        # use cursor to execute sql query, get user and password
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=%s;", (username,))
-        user_row = cursor.fetchone()
-        cursor.close()
+        # Get the row with the same username as entered
+        user_row = getUserRow(username)
 
-        print(user_row)
-
-        # If there is no row with the entered username
+        # If username exist
         if user_row:
-            db_user_id = user_row[0]
-            db_name = user_row[1]
-            db_pword = user_row[2]
 
-            if (username == db_name) & (pword == db_pword):
-                # user redirect to send user to a different page, while use render_template to render content for a page, url will not change when render_template
+            # retrieve data from tuple
+            db_user_id, _, db_pword = user_row
+
+            # Check if user input password match the hashed pw in db
+            if bcrypt.checkpw(pword,db_pword.encode('utf-8')):
+
+                # Password matches, redirect to manage page
                 return redirect(url_for('manage', user_id=db_user_id))
-        else:
-            return render_template('login.html', message='Invalid Username')
+            
+        # If there is no user_row or password does not match
+        message="Invalid Username or Password"    
+    
+    # GET request
+    return render_template('login.html', message=message)
 
-    # IF Get request      
-    return render_template('login.html', message='Login')
 
 @app.route('/manage/<user_id>', methods=['GET', 'POST'])
 def manage(user_id):
     return render_template('manage.html',documents=getDocRows(user_id))
+
+@app.route('/signup', methods=["GET","POST"])
+def signup():
+    nameError = ""
+    pwError = ""
+    if request.method == 'POST':
+        # Get user input
+        username = request.form['username']
+        password = request.form['password']
+        confirm = request.form['confirm']
+        
+        user_row = getUserRow(username)
+
+        # if username does not exist in db
+        if (not user_row) and (password == confirm):
+            addUser(username,password)
+            return redirect(url_for('signup_success'))
+        elif (user_row):
+            nameError = "Username already exist. Please try another name"
+        elif not (password == confirm):
+            pwError = "Password does not match. Try Again"
+
+    return render_template('signup.html', nameError=nameError, pwError = pwError)
+
+@app.route('/signup_success',methods=["GET","POST"])
+def signup_success():
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    return render_template('signup_success.html')
 
 def user_exist(users,name,pword):
     for user in users:
@@ -54,7 +84,6 @@ def user_exist(users,name,pword):
         else: 
             print("NoUserExist")
             return False
-        
 
 def getDocRows(user_id):
     cursor = mysql.connection.cursor()
@@ -62,6 +91,33 @@ def getDocRows(user_id):
     doc_rows = cursor.fetchall()
     cursor.close()
     return doc_rows
+
+def getUserRow(username):
+    # use cursor to execute sql query, get user and password
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=%s;", (username,))
+    user_row = cursor.fetchone()
+    cursor.close()
+
+    if not user_row:
+        # Handle case when user doesn't exist
+        return None
+    return user_row
+
+# Will hash the password, and insert the username and hashed password into the users table
+def addUser(username,pword):
+    cursor = mysql.connection.cursor()
+    hashed = hash(pword)
+    cursor.execute("INSERT INTO users (username, pword) VALUES (%s,%s)",(username,hashed))
+    # Update the table
+    mysql.connection.commit()
+    cursor.close
+
+def hash(pword):
+    # generate salt
+    s = bcrypt.gensalt()
+    # turn the password into byte
+    return bcrypt.hashpw(pword.encode('utf-8'), salt=s)
 
 if __name__ == '__main__':
     app.run(debug=True)
